4dee66140084246da46e9bdbe49c4aaf676d97d4
Andrew Hearin <andrew.hearin@yale.edu>
2016-03-05 10:03:06 -0500

    Discovered bug in pairwise velocity statistics. Looks potentially serious. PBCs look like they are not being handled correctly with the velocities.
    In the halotools/mock_observables/tests/test_pairwise_velocity_stats.py testing module, the function test_mean_radial_velocity_vs_r_correctness_pbc2 fails. The docstring of this function explains the test. Here is the traceback. It's a simple code-correctness test.
    
        @pytest.mark.slow
        def test_mean_radial_velocity_vs_r_correctness_pbc2():
        	""" Create two tight localizations of points,
        	one at (0.5, 0.5, 0.05), the other at (0.5, 0.5, 0.9).
        	The first set of points is moving at -50 in the z-direction,
        	the second set of points is at rest.
        	Verify that mean_radial_velocity_vs_r returns -50
        	for the outer bin.
    
        	Same test as test_mean_radial_velocity_vs_r_correctness_pbc,
        	only here the PBC should matter.
        	"""
        	np.random.seed(43)
    
        	npts = 200
        	sample1 = generate_locus_of_3d_points(npts,
        		xc=0.5, yc=0.5, zc=0.05, epsilon = 0.0001)
        	velocities1 = np.zeros(npts*3).reshape(npts, 3)
        	velocities1[:,2] = -50.
    
        	sample2 = generate_locus_of_3d_points(npts,
        		xc=0.5, yc=0.5, zc=0.9, epsilon = 0.0001)
        	velocities2 = np.zeros(npts*3).reshape(npts, 3)
    
        	rbins = np.array([0, 0.1, 0.3])
        	s1s1, s1s2, s2s2 = mean_radial_velocity_vs_r(sample1, velocities1, rbins,
        		sample2 = sample2, velocities2 = velocities2, period=1.)
    
        	assert np.allclose(s1s1[0], 0, rtol=0.01)
        	assert np.allclose(s2s2[0], 0, rtol=0.01)
    >   	assert np.allclose(s1s2[1], -50, rtol=0.01)
    E    assert <function allclose at 0x1007d8500>(32.382414112854001, -50, rtol=0.01)
    E     +  where <function allclose at 0x1007d8500> = np.allclose
    
    halotools/mock_observables/tests/test_pairwise_velocity_stats.py:455: AssertionError

--- halotools/mock_observables/tests/test_pairwise_velocity_stats.py
+++ halotools/mock_observables/tests/test_pairwise_velocity_stats.py
@@ -361,3 +361,96 @@ def test_los_pvd_vs_rp_cross_consistency():
 
 	assert np.allclose(s1s2a,s1s2b, rtol=0.001)
 
+@pytest.mark.slow
+def test_mean_radial_velocity_vs_r_correctness():
+	""" Create two tight localizations of points, 
+	one at (0.5, 0.5, 0.1), the other at (0.5, 0.5, 0.2). 
+	The first set of points is moving at -50 in the z-direction, 
+	the second set of points is at rest. 
+	Verify that mean_radial_velocity_vs_r returns -50 
+	for the outer bin. 
+	"""
+	np.random.seed(43)
+
+	npts = 200
+	sample1 = generate_locus_of_3d_points(npts, 
+		xc=0.5, yc=0.5, zc=0.1, epsilon = 0.0001)
+	velocities1 = np.zeros(npts*3).reshape(npts, 3)
+	velocities1[:,2] = 50.
+
+	sample2 = generate_locus_of_3d_points(npts, 
+		xc=0.5, yc=0.5, zc=0.25, epsilon = 0.0001)
+	velocities2 = np.zeros(npts*3).reshape(npts, 3)
+
+	rbins = np.array([0, 0.1, 0.3])
+	s1s1, s1s2, s2s2 = mean_radial_velocity_vs_r(sample1, velocities1, rbins, 
+		sample2 = sample2, velocities2 = velocities2)
+
+	assert np.allclose(s1s1[0], 0, rtol=0.01)
+	assert np.allclose(s2s2[0], 0, rtol=0.01)
+	assert np.allclose(s1s2[1], -50, rtol=0.01)
+
+@pytest.mark.slow
+def test_mean_radial_velocity_vs_r_correctness_pbc():
+	""" Create two tight localizations of points, 
+	one at (0.5, 0.5, 0.1), the other at (0.5, 0.5, 0.2). 
+	The first set of points is moving at -50 in the z-direction, 
+	the second set of points is at rest. 
+	Verify that mean_radial_velocity_vs_r returns -50 
+	for the outer bin. 
+	Same test as test_mean_radial_velocity_vs_r_correctness, 
+	only here we apply PBC, which should not matter in this case. 
+	"""
+	np.random.seed(43)
+
+	npts = 200
+	sample1 = generate_locus_of_3d_points(npts, 
+		xc=0.5, yc=0.5, zc=0.1, epsilon = 0.0001)
+	velocities1 = np.zeros(npts*3).reshape(npts, 3)
+	velocities1[:,2] = 50.
+
+	sample2 = generate_locus_of_3d_points(npts, 
+		xc=0.5, yc=0.5, zc=0.25, epsilon = 0.0001)
+	velocities2 = np.zeros(npts*3).reshape(npts, 3)
+
+	rbins = np.array([0, 0.1, 0.3])
+	s1s1, s1s2, s2s2 = mean_radial_velocity_vs_r(sample1, velocities1, rbins, 
+		sample2 = sample2, velocities2 = velocities2, period=1.)
+
+	assert np.allclose(s1s1[0], 0, rtol=0.01)
+	assert np.allclose(s2s2[0], 0, rtol=0.01)
+	assert np.allclose(s1s2[1], -50, rtol=0.01)
+
+
+@pytest.mark.slow
+def test_mean_radial_velocity_vs_r_correctness_pbc2():
+	""" Create two tight localizations of points, 
+	one at (0.5, 0.5, 0.05), the other at (0.5, 0.5, 0.9). 
+	The first set of points is moving at -50 in the z-direction, 
+	the second set of points is at rest. 
+	Verify that mean_radial_velocity_vs_r returns -50 
+	for the outer bin. 
+
+	Same test as test_mean_radial_velocity_vs_r_correctness_pbc, 
+	only here the PBC should matter. 
+	"""
+	np.random.seed(43)
+
+	npts = 200
+	sample1 = generate_locus_of_3d_points(npts, 
+		xc=0.5, yc=0.5, zc=0.05, epsilon = 0.0001)
+	velocities1 = np.zeros(npts*3).reshape(npts, 3)
+	velocities1[:,2] = -50.
+
+	sample2 = generate_locus_of_3d_points(npts, 
+		xc=0.5, yc=0.5, zc=0.9, epsilon = 0.0001)
+	velocities2 = np.zeros(npts*3).reshape(npts, 3)
+
+	rbins = np.array([0, 0.1, 0.3])
+	s1s1, s1s2, s2s2 = mean_radial_velocity_vs_r(sample1, velocities1, rbins, 
+		sample2 = sample2, velocities2 = velocities2, period=1.)
+
+	assert np.allclose(s1s1[0], 0, rtol=0.01)
+	assert np.allclose(s2s2[0], 0, rtol=0.01)
+	assert np.allclose(s1s2[1], -50, rtol=0.01)
+
