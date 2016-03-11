0f34a2fdf7c14046c21020db39ea1bdc50a13769
Andrew Hearin <andrew.hearin@yale.edu>
2016-03-10 19:09:29 -0500

    Demonstrate failure of conditional isolation cond_func=5

--- halotools/mock_observables/tests/test_isolation_criteria.py
+++ halotools/mock_observables/tests/test_isolation_criteria.py
@@ -80,11 +80,6 @@ def test_cylindrical_isolation3():
 	iso = cylindrical_isolation(sample1, sample2, rp_max, pi_max, period=1)
 	assert np.all(iso == True)
 
-# def conditional_spherical_isolation(sample1, sample2, r_max,
-#                         marks1, marks2, cond_func, period=None,
-#                         num_threads=1, approx_cell1_size=None, approx_cell2_size=None):
-
-
 def test_conditional_spherical_isolation_cond_func1():
 	sample1 = generate_locus_of_3d_points(10, xc=0.05, yc=0.05, zc=0.05)
 	sample2 = generate_locus_of_3d_points(10, xc=0.95, yc=0.95, zc=0.95)
@@ -151,10 +146,43 @@ def test_conditional_spherical_isolation_cond_func4():
 		marks1, marks1, cond_func, period=1)
 	assert np.all(marked_iso4 == True)
 
+def test_conditional_spherical_isolation_cond_func5():
+	sample1 = generate_locus_of_3d_points(10, xc=0.05, yc=0.05, zc=0.05)
+	sample2 = generate_locus_of_3d_points(10, xc=0.95, yc=0.95, zc=0.95)
+	r_max = 0.25
 
+	# First verify that the two points are not isolated when ignoring the marks
+	iso = spherical_isolation(sample1, sample2, r_max, period=1)
+	assert np.all(iso == False)
 
+	# The first sample1 mark is 1, the second sample1 mark is also 1
+	# All sample2 marks are 0
+	# Thus w_1[0] > (w_2[0]+w_1[1]) NEVER holds, 
+	# and so the marked isolation should always be True
+	marks1a, marks1b = np.ones(len(sample1)), np.ones(len(sample1)) 
+	marks1 = np.vstack([marks1a, marks1b]).T 
+	marks2a, marks2b = np.zeros(len(sample2)), np.zeros(len(sample2)) 
+	marks2 = np.vstack([marks2a, marks2b]).T 
+	
+	cond_func = 5
+	marked_iso5 = conditional_spherical_isolation(sample1, sample2, r_max, 
+		marks1, marks2, cond_func, period=1)
+	assert np.all(marked_iso5 == True)
 
-
+	# The first sample1 mark is 1, the second sample1 mark is 0
+	# All sample2 marks are 0
+	# Thus w_1[0] > (w_2[0]+w_1[1]) ALWAYS holds, 
+	# and so the marked isolation should be equivalent to the unmarked isolation 
+	# This test fails
+	marks1a, marks1b = np.ones(len(sample1)), np.zeros(len(sample1)) 
+	marks1 = np.vstack([marks1a, marks1b]).T 
+	marks2a, marks2b = np.zeros(len(sample2)), np.zeros(len(sample2)) 
+	marks2 = np.vstack([marks2a, marks2b]).T 
+	
+	cond_func = 5
+	marked_iso5 = conditional_spherical_isolation(sample1, sample2, r_max, 
+		marks1, marks2, cond_func, period=1)
+	assert np.all(marked_iso5 == False)
 
 
 
