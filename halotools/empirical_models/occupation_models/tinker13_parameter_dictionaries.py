0c78f5d26eed21f70128d54f2b9535ea2f744878
Andrew Hearin <andrew.hearin@yale.edu>
2017-03-30 10:27:54 -0400

    Added dedicated module to tinker13 parameters

---
+++ halotools/empirical_models/occupation_models/tinker13_parameter_dictionaries.py
@@ -0,0 +1,41 @@
+"""
+"""
+
+param_dict_z1 = dict()
+
+# active centrals
+param_dict_z1['smhm_m1_0_active'] = 12.56
+param_dict_z1['smhm_m0_0_active'] = 10.96
+param_dict_z1['smhm_beta_0_active'] = 0.44
+param_dict_z1['smhm_delta_0_active'] = 0.52
+param_dict_z1['smhm_gamma_0_active'] = 1.48
+param_dict_z1['scatter_model_param1_active'] = 0.21
+
+# active satellites
+param_dict_z1['bcut_active'] = 0.28
+param_dict_z1['bsat_active'] = 33.96
+param_dict_z1['betacut_active'] = 0.77
+param_dict_z1['betasat_active'] = 1.05
+param_dict_z1['alphasat_active'] = 0.99
+
+# quiescent centrals
+param_dict_z1['smhm_m1_0_quiescent'] = 12.08
+param_dict_z1['smhm_m0_0_quiescent'] = 10.7
+param_dict_z1['smhm_beta_0_quiescent'] = 0.32
+param_dict_z1['smhm_delta_0_quiescent'] = 0.93
+param_dict_z1['smhm_gamma_0_quiescent'] = 0.81
+param_dict_z1['scatter_model_param1_quiescent'] = 0.28
+
+# quiescent satellites
+param_dict_z1['bcut_quiescent'] = 21.42
+param_dict_z1['bsat_quiescent'] = 17.9
+param_dict_z1['betacut_quiescent'] = -0.12
+param_dict_z1['betasat_quiescent'] = 0.62
+param_dict_z1['alphasat_quiescent'] = 1.08
+
+# quiescent central fraction
+param_dict_z1['smhm_m1_0_quiescent'] = 12.08
+param_dict_z1['smhm_m0_0_quiescent'] = 10.7
+param_dict_z1['smhm_beta_0_quiescent'] = 0.32
+param_dict_z1['smhm_delta_0_quiescent'] = 0.93
+param_dict_z1['smhm_gamma_0_quiescent'] = 0.81
