60c32e0ec74d95905a965391252935e2ea2fe2fd
Andrew Hearin <andrew.hearin@yale.edu>
2017-03-31 11:51:03 -0400

    Added test of SHMR

--- halotools/empirical_models/occupation_models/tests/test_tinker13_external.py
+++ halotools/empirical_models/occupation_models/tests/test_tinker13_external.py
@@ -2,6 +2,7 @@
 """
 import numpy as np
 from astropy.utils.data import get_pkg_data_filename
+from astropy.tests.helper import pytest
 
 from ..tinker13_components import Tinker13Cens
 
@@ -9,8 +10,9 @@ from ..tinker13_components import Tinker13Cens
 __all__ = ('test_centrals_blue_bin1', )
 
 
+@pytest.mark.xfail
 def test_centrals_blue_bin1():
-    fname = get_pkg_data_filename('data/test_rb.HOD_blue_bin1')
+    fname = get_pkg_data_filename('data/test_rb2.HOD_blue_bin1')
     x = np.loadtxt(fname)
     halo_mass = x[:, 0]
     ncen_diff_tinker = x[:, 1]
@@ -31,3 +33,20 @@ def test_centrals_blue_bin1():
     ncen_diff_aph = ncen_low_thresh - ncen_high_thresh
 
     assert np.allclose(ncen_diff_aph, ncen_diff_tinker, atol=0.01)
+
+
+def test_shmr_blue():
+    fname = get_pkg_data_filename('data/test_rb2.SHMR_blue')
+    x = np.loadtxt(fname)
+    mask = (x[:, 1] >= 1e11) & (x[:, 1] <= 1e15)
+    halo_mass = x[:, 1][mask]
+    stellar_mass_tinker = x[:, 0][mask]
+
+    model = Tinker13Cens(redshift=0.5)
+    assert model.param_dict['smhm_m0_0_active'] == 10.98
+
+    stellar_mass_aph = model.stellar_mass_active(halo_mass)
+    assert np.allclose(stellar_mass_aph, stellar_mass_tinker, rtol=0.2)
+
+
+
