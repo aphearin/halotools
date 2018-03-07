9173a1651cd8ba4cd7865d3516dadc6deb62f393
Andrew Hearin <andrew.hearin@yale.edu>
2018-03-07 08:52:36 -0600

    Added assume_x_is_sorted feature to python wrapper

--- halotools/empirical_models/abunmatch/bin_free_cam.py
+++ halotools/empirical_models/abunmatch/bin_free_cam.py
@@ -5,7 +5,8 @@ from ...utils import unsorting_indices
 from .engines import cython_bin_free_cam_kernel
 
 
-def bin_free_conditional_abunmatch(x, y, x2, y2, nwin):
+def bin_free_conditional_abunmatch(x, y, x2, y2, nwin,
+            assume_x_is_sorted=False, assume_x2_is_sorted=False):
     """
     Examples
     --------
@@ -17,17 +18,28 @@ def bin_free_conditional_abunmatch(x, y, x2, y2, nwin):
     >>> nwin = 51
     >>> result = bin_free_conditional_abunmatch(x, y, x2, y2, nwin)
     """
-    idx_x_sorted = np.argsort(x)
-    x_sorted = x[idx_x_sorted]
-    y_sorted = y[idx_x_sorted]
+    if assume_x_is_sorted:
+        x_sorted = x
+        y_sorted = y
+    else:
+        idx_x_sorted = np.argsort(x)
+        x_sorted = x[idx_x_sorted]
+        y_sorted = y[idx_x_sorted]
 
-    idx_x2_sorted = np.argsort(x2)
-    x2_sorted = x2[idx_x2_sorted]
-    y2_sorted = y2[idx_x2_sorted]
+    if assume_x2_is_sorted:
+        x2_sorted = x2
+        y2_sorted = y2
+    else:
+        idx_x2_sorted = np.argsort(x2)
+        x2_sorted = x2[idx_x2_sorted]
+        y2_sorted = y2[idx_x2_sorted]
 
     i2_matched = np.searchsorted(x2_sorted, x_sorted).astype('i4')
 
     result = np.array(cython_bin_free_cam_kernel(
         y_sorted, i2_matched, x2_sorted, y2_sorted, nwin))
 
-    return result[unsorting_indices(idx_x_sorted)]
+    if assume_x_is_sorted:
+        return result
+    else:
+        return result[unsorting_indices(idx_x_sorted)]
