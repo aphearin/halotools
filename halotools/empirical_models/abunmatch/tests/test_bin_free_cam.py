961130fc4081c36641e31a715456da15a781db44
Andrew Hearin <andrew.hearin@yale.edu>
2018-03-07 09:05:08 -0600

    Added elementary unit-test

--- halotools/empirical_models/abunmatch/tests/test_bin_free_cam.py
+++ halotools/empirical_models/abunmatch/tests/test_bin_free_cam.py
@@ -9,4 +9,22 @@ fixed_seed = 43
 
 
 def test1():
-    pass
+    nwin = 3
+
+    x = [1, 2, 3, 4, 5, 6, 7]
+    x2 = [2.5, 3.5, 4.5, 5.5, 6.5]
+
+    y = np.arange(0, len(x))
+    y2 = np.arange(5, len(x2)+5)
+
+    i2_matched = np.searchsorted(x2, x)
+
+    result = bin_free_conditional_abunmatch(x, y, x2, y2, nwin)
+
+    print("i  = {0}\n".format(i2_matched.astype('f4')))
+
+    print("y  = {0}".format(y))
+    print("y2 = {0}\n".format(y2))
+    print("r  = {0}".format(result))
+
+    assert 4 == 5
