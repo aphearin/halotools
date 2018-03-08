476ae0d3e38defc677e60db42dfe44f8627eadb4
Andrew Hearin <andrew.hearin@yale.edu>
2018-03-08 16:30:55 -0600

    Added hard-coded values for x, x2, y, y2 to test3

--- halotools/empirical_models/abunmatch/tests/test_bin_free_cam.py
+++ halotools/empirical_models/abunmatch/tests/test_bin_free_cam.py
@@ -106,18 +106,17 @@ def test2():
 
 
 def test3():
-    """ Test case where x and x2 are sorted, but the nearest x2 value
-    is no longer simple
+    """ Test case where x and x2 are sorted, y and y2 are sorted,
+    but the nearest x--x2 correspondence is no longer simple
     """
     nwin = 3
     nhalfwin = int(nwin/2)
 
-    x = np.sort(np.random.rand(10))
-    x2 = np.sort(np.random.uniform(0.5, 0.6, 10))
+    x = np.array([0.1,  0.36, 0.36, 0.74, 0.83])
+    x2 = np.array([0.54, 0.54, 0.55, 0.56, 0.57])
 
-    with NumpyRNGContext(fixed_seed):
-        y = np.round(np.random.rand(len(x)), 2)
-        y2 = np.round(np.random.rand(len(x2)), 2)
+    y = np.array([0.12, 0.13, 0.24, 0.33, 0.61])
+    y2 = np.array([0.03, 0.54, 0.67, 0.73, 0.86])
 
     i2_matched = np.searchsorted(x2, x)
     i2_matched = np.where(i2_matched >= len(y2), len(y2)-1, i2_matched)
