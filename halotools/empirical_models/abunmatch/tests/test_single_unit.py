e4fcd55b6255368bd9dc49022d00f28de031f661
Andrew Hearin <andrew.hearin@yale.edu>
2018-03-10 14:18:43 -0600

    Added small failing unit test to test_single_unit

--- halotools/empirical_models/abunmatch/tests/test_single_unit.py
+++ halotools/empirical_models/abunmatch/tests/test_single_unit.py
@@ -1,10 +1,31 @@
 """
 """
 import numpy as np
+from astropy.utils.misc import NumpyRNGContext
 from ..bin_free_cam import bin_free_conditional_abunmatch
 from ....utils.conditional_percentile import cython_sliding_rank, rank_order_function
+from .test_pure_python import pure_python_rank_matching
 
 
-def test_function():
-    pass
+fixed_seed = 43
 
+
+def test4():
+
+    n1, n2, nwin = 8, 8, 3
+    x = np.linspace(0, 1, n1)
+    with NumpyRNGContext(fixed_seed):
+        y = np.random.uniform(0, 1, n1)
+    ranks_sample1 = cython_sliding_rank(x, y, nwin)
+
+    x2 = np.linspace(0, 1, n2)
+    with NumpyRNGContext(fixed_seed):
+        y2 = np.random.uniform(-4, -3, n2)
+    ranks_sample2 = cython_sliding_rank(x2, y2, nwin)
+
+    pure_python_result = pure_python_rank_matching(x, ranks_sample1,
+            x2, ranks_sample2, y2, nwin)
+
+    result = bin_free_conditional_abunmatch(x, y, x2, y2, nwin)
+
+    assert np.allclose(result, pure_python_result)
