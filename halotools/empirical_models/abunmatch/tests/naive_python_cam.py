dedd948e81e8fd834072d48109be3a00bd35efd1
Andrew Hearin <andrew.hearin@yale.edu>
2018-03-11 10:16:56 -0600

    Fixed bugs in pure python window function.

--- halotools/empirical_models/abunmatch/tests/naive_python_cam.py
+++ halotools/empirical_models/abunmatch/tests/naive_python_cam.py
@@ -3,23 +3,20 @@
 import numpy as np
 
 
-def sample2_window_indices(x1, x_sample2, nwin):
+def sample2_window_indices(ix1, x_sample1, x_sample2, nwin):
     nhalfwin = int(nwin/2)
     npts2 = len(x_sample2)
 
-    iy2 = min(np.searchsorted(x_sample2, x1), len(x_sample2)-1)
+    x1 = x_sample1[ix1]
+    iy2 = min(np.searchsorted(x_sample2, x1), npts2-1)
 
-    init_iy2_low = iy2 - nhalfwin
-    init_iy2_high = init_iy2_low+nwin
-
-    if init_iy2_low < 0:
-        init_iy2_low = 0
-        init_iy2_high = init_iy2_low + nwin
-        iy2 = init_iy2_low + nhalfwin
-    elif init_iy2_high > npts2 - nhalfwin:
-        init_iy2_high = npts2
-        init_iy2_low = init_iy2_high - nwin
-        iy2 = init_iy2_low + nhalfwin
+    if iy2 <= nhalfwin:
+        init_iy2_low, init_iy2_high = 0, nwin
+    elif iy2 >= npts2 - nhalfwin - 1:
+        init_iy2_low, init_iy2_high = npts2-nwin, npts2
+    else:
+        init_iy2_low = iy2 - nhalfwin
+        init_iy2_high = init_iy2_low+nwin
 
     return init_iy2_low, init_iy2_high
 
@@ -31,13 +28,9 @@ def pure_python_rank_matching(x_sample1, ranks_sample1,
     result = np.zeros_like(x_sample1)
 
     n1 = len(x_sample1)
-    n2 = len(x_sample2)
-    nhalfwin = int(nwin/2)
 
     for i in range(n1):
-        x1 = x_sample1[i]
-        low, high = sample2_window_indices(x1, x_sample2, nwin)
-
+        low, high = sample2_window_indices(i, x_sample1, x_sample2, nwin)
         sorted_window = np.sort(y_sample2[low:high])
         rank1 = ranks_sample1[i]
         result[i] = sorted_window[rank1]
