1a66c49664aae16b0887f7c3be9c52a93e51cc5b
Andrew Hearin <andrew.hearin@yale.edu>
2018-03-11 09:20:40 -0600

    Corrected error in test_hard_coded_case5. Pure python implementation now fails this test

--- halotools/empirical_models/abunmatch/tests/test_bin_free_cam.py
+++ halotools/empirical_models/abunmatch/tests/test_bin_free_cam.py
@@ -262,17 +262,24 @@ def test_hard_coded_case4():
     assert np.allclose(result, correct_result)
 
 
-def test_hard_coded_case5():
-    nwin = 3
+def test_hard_coded_case6():
+    """
+    """
 
-    x = np.array((1., 1., 1, 1, 1))
-    x2 = np.array([0.1,  0.36, 0.5, 0.74, 0.83])
+    x = [0.15, 0.31, 0.48, 0.64, 0.81, 0.97, 1.14, 1.3]
+    x2 = [0.15, 0.38, 0.61, 0.84, 1.07, 1.3]
 
-    y = np.array([0.12, 0.13, 0.24, 0.33, 0.61])
-    y2 = np.array([0.3, 0.04, 0.6, 10., 5.])
+    y = [0.22, 0.87, 0.21, 0.92, 0.49, 0.61, 0.77, 0.52]
+    y2 = [-3.78, -3.13, -3.79, -3.08, -3.51, -3.39]
 
-    correct_result = [0.6, 5, 5, 5, 10]
+    nwin = 5
+
+    ranks_sample1 = cython_sliding_rank(x, y, nwin)
+    ranks_sample2 = cython_sliding_rank(x2, y2, nwin)
+
+    pure_python_result = pure_python_rank_matching(x, ranks_sample1,
+            x2, ranks_sample2, y2, nwin)
+
     result = bin_free_conditional_abunmatch(x, y, x2, y2, nwin)
 
-    assert np.allclose(result, correct_result)
-
+    assert np.allclose(result, pure_python_result)
