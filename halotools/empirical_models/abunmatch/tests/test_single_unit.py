c42e356d2248a6929b681ad0a6178863d116d2dc
Andrew Hearin <andrew.hearin@yale.edu>
2018-03-11 09:27:26 -0600

    Migrated pure python to its own module

--- halotools/empirical_models/abunmatch/tests/test_single_unit.py
+++ halotools/empirical_models/abunmatch/tests/test_single_unit.py
@@ -4,7 +4,7 @@ import numpy as np
 from astropy.utils.misc import NumpyRNGContext
 from ..bin_free_cam import bin_free_conditional_abunmatch
 from ....utils.conditional_percentile import cython_sliding_rank, rank_order_function
-from .test_pure_python import pure_python_rank_matching
+from .naive_python_cam import pure_python_rank_matching
 
 
 fixed_seed = 5
