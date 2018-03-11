a171aeae51fc782e552932fbe0b5d2f953368d05
Andrew Hearin <andrew.hearin@yale.edu>
2018-03-11 17:35:05 -0600

    Added forgotten import in __init__ module of abunmatch sub-package

--- halotools/empirical_models/abunmatch/__init__.py
+++ halotools/empirical_models/abunmatch/__init__.py
@@ -1,3 +1,4 @@
 from .conditional_abunmatch_bin_based import *
 from .noisy_percentile import *
 from .engines import cython_bin_free_cam_kernel
+from .conditional_abunmatch import conditional_abunmatch
