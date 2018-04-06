7c5e702a57f8918a24202ef3539d5a3ba9c99748
Andrew Hearin <andrew.hearin@yale.edu>
2018-04-06 15:29:06 -0500

    Added new failing unit tests

--- halotools/empirical_models/abunmatch/tests/test_cam_indices.py
+++ halotools/empirical_models/abunmatch/tests/test_cam_indices.py
@@ -28,7 +28,7 @@ def test_cam_indices_equals_cam():
 
 
 @pytest.mark.xfail
-def test_cam_indices_agrees_with_cam():
+def test_cam_indices_agrees_with_cam_interior_points():
     """ Upshot behavior testing of cam_indices function.
     """
     nwin = 51
@@ -41,4 +41,30 @@ def test_cam_indices_agrees_with_cam():
     ynew1 = conditional_abunmatch(x, y, x2, y2, nwin, add_subgrid_noise=False)
     indx_ynew1 = conditional_abunmatch_indices(x, y, x2, y2, nwin, add_subgrid_noise=False)
 
-    assert np.allclose(ynew1, y2[indx_ynew1], rtol=0.05)
+    nhalfwin = int(nwin/2)
+    correct_interior_points = ynew1[nhalfwin:-nhalfwin]
+    returned_interior_points = y2[indx_ynew1.astype('i8')][nhalfwin:-nhalfwin]
+    assert np.allclose(correct_interior_points, returned_interior_points, rtol=0.05)
+
+
+@pytest.mark.xfail
+def test_cam_indices_agrees_with_cam_left_endpoint():
+    """ Upshot behavior testing of cam_indices function for left endpoint.
+    """
+    nwin = 51
+    npts = int(1e4)
+    with NumpyRNGContext(fixed_seed):
+        x = np.random.uniform(0, 4, npts)
+        x2 = np.random.uniform(-1, 10, npts)
+        y = np.random.normal(loc=0, scale=4, size=npts)
+        y2 = np.random.normal(loc=-2, scale=0.4, size=npts)
+    ynew1 = conditional_abunmatch(x, y, x2, y2, nwin, add_subgrid_noise=False)
+    indx_ynew1 = conditional_abunmatch_indices(
+        x, y, x2, y2, nwin, add_subgrid_noise=False).astype('i8')
+
+    nhalfwin = int(nwin/2)
+    correct_left_endpoint = ynew1[:nhalfwin+1]
+    left_endpoint = y2[indx_ynew1][:nhalfwin+1]
+
+    assert np.allclose(correct_left_endpoint, left_endpoint, rtol=0.05)
+
