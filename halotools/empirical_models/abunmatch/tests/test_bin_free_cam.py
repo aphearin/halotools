094fac0495c378ee8f459baa584153c75ca18fc7
Andrew Hearin <andrew.hearin@yale.edu>
2018-04-06 14:40:33 -0500

    Renamed bin_free_cam.py module to cam.py

--- halotools/empirical_models/abunmatch/tests/test_bin_free_cam.py
+++ halotools/empirical_models/abunmatch/tests/test_bin_free_cam.py
@@ -2,7 +2,7 @@
 """
 import numpy as np
 from astropy.utils.misc import NumpyRNGContext
-from ..bin_free_cam import conditional_abunmatch
+from ..cam import conditional_abunmatch
 from ....utils.conditional_percentile import cython_sliding_rank, rank_order_function
 from .naive_python_cam import pure_python_rank_matching
 from ....utils import unsorting_indices
--- halotools/empirical_models/abunmatch/tests/test_single_unit.py
+++ halotools/empirical_models/abunmatch/tests/test_single_unit.py
@@ -2,7 +2,7 @@
 """
 import numpy as np
 from astropy.utils.misc import NumpyRNGContext
-from ..bin_free_cam import conditional_abunmatch
+from ..cam import conditional_abunmatch
 
 
 fixed_seed = 5
